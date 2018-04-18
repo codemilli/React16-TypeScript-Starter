728f0ec5a95945301b1a5f7719bbdd2ae1524a4f
hckrmoon <hckrmoon@gmail.com>
2018-04-19 00:35:14 +0900

    fix useless codes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ if (module.hot) {
   module.hot.accept()
 }
 
-// Hello123 123 12312 3
-
 async function render(): Promise<void> {
   const {Home} = await import(/* webpackChunkName: "home" */ './pages/Home')
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import React, {SFC} from 'react';
-import * as _ from 'lodash'
-
 import {toNumber} from '../utils/Number';
-
-console.log('lodash : ', _.map)
 
 interface Props {
   title: string
@@ -11,6 +7,6 @@ interface Props {
 
 export const Home: SFC<Props> = ({title}) => (
   <div>
-    <h2>{title} Home !!! {toNumber('1')}</h2>
+    <h2>{title} Home !!!!! {toNumber('1')}</h2>
   </div>
 )

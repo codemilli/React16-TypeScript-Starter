f21e4795c95bd95275a8212c603007e9f2c20978
hckrmoon <hckrmoon@gmail.com>
2018-04-19 23:26:36 +0900

    set redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import {NotFound} from './pages/NotFound';
 import {Home} from './pages/Home';
 
@@ -8,13 +8,14 @@ if (module.hot) {
   module.hot.accept()
 }
 
-const prefixLanguage = location.pathname.split('/')[1] || ''
+const prefixLanguage = location.pathname.split('/')[1] || 'ko'
 const root = document.getElementById('root')
 
 ReactDOM.render((
   <BrowserRouter>
     <Switch>
       <Route path={`/${prefixLanguage}`} component={Home} />
+      <Redirect from="" to={`/${prefixLanguage}`} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
